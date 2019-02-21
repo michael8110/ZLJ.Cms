c92e237ea33cda37b065febf2ed77cddee96a891
zhenglijun <476647235@qq.com>
2019-02-21 18:01:42 +0800

    示例项目代码

---
+++ Sample02/Myweb/Myweb/Controllers/ContentController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Myweb.Models;
+
+namespace Myweb.Controllers
+{  
+   
+    public class ContentController : Controller
+    {
+        /// <summary>
+        /// 首页显示
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Index()
+        {
+            var contents = new List<Content>();
+            for (int i = 1; i < 200; i++)
+            {
+                contents.Add(new Content { Id = i, title = $"{i}的标题", content = $"{i}的内容", status = 1, add_time = DateTime.Now.AddDays(-i) });
+            }
+            return View(new ContentViewModel { Contents = contents });
+
+        }
+    }
+}
